0aace4fa037979cb7216e4f52c9c753dc455c6d6
vpopi4 <vpopi4@yandex.ru>
2025-02-05 20:50:16 +0300

    feat: enhance HitDto with validation annotations for app, uri, ip, and timestamp

--- ewm-stats-service/stats-dto/src/main/java/ru/practicum/ewm/stats/dto/request/HitDto.java
+++ ewm-stats-service/stats-dto/src/main/java/ru/practicum/ewm/stats/dto/request/HitDto.java
@@ -1,6 +1,10 @@
 package ru.practicum.ewm.stats.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,10 +18,21 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class HitDto {
+    @NotBlank
+    @Size(max = 30)
     private String app;
+
+    @NotBlank
+    @Size(max = 50)
     private String uri;
+
     @ValidIpAddress
+    @NotBlank
+    @Size(max = 39)
     private String ip;
+
+    @NotNull
+    @PastOrPresent
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
